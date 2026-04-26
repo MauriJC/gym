45c1226731df8c7fe9a92d167cf4141bcaa1ef8d
MauriJC <xaviermauricio33@gmail.com>
2026-04-26 19:22:43 -0300

    feat: update tab layout and add new screens for dashboard and historial

---
+++ app/(tabs)/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ThemedText } from "@/components/themed-text";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Dashboard = () => {
+  return (
+    <SafeAreaView>
+      <ThemedText>Dashboard</ThemedText>
+    </SafeAreaView>
+  );
+};
+
+export default Dashboard;
---
+++ app/(tabs)/historial.tsx
@@ -0,0 +1,13 @@
+import { ThemedText } from "@/components/themed-text";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Historial = () => {
+  return (
+    <SafeAreaView>
+      <ThemedText>Historial</ThemedText>
+    </SafeAreaView>
+  );
+};
+
+export default Historial;
