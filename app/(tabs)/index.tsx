15d577f68d0ecb27cfa81b243814051ef97b66a2
MauriJC <xaviermauricio33@gmail.com>
2026-04-26 19:29:52 -0300

    refactor: update HomeScreen layout and styles for improved structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,33 @@
-import { StyleSheet } from "react-native";
-import { HelloWave } from "@/components/hello-wave";
+import { StyleSheet, View } from "react-native";
 import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
-import { Link } from "expo-router";
+//import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Bienvenido a gym!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>Aqui vamos a poner las secciones de la app.</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleContainer}>
+        <ThemedText type="title">Gym tracker app!</ThemedText>
+      </View>
+      {/*  
+      Dejo comentado esto para usarlo de guia despues
+      <View style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
             <ThemedText type="subtitle">Step 2: Explore</ThemedText>
           </Link.Trigger>
         </Link>
-      </ThemedView>
+      </View> */}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   titleContainer: {
     flexDirection: "row",
     alignItems: "center",
@@ -36,12 +36,5 @@ const styles = StyleSheet.create({
   stepContainer: {
     gap: 8,
     marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
   },
 });
