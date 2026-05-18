baef0196749d8c0ee95a8bb7619293061f3d7c1b
MauriJC <xaviermauricio33@gmail.com>
2026-05-17 23:02:02 -0300

    chore (tabs): change name to Sesion activa

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -43,7 +43,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="sessions"
         options={{
-          title: "Sesiones",
+          title: "Sesión activa",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="dumbbell" size={24} color={color} />
           ),
